1dab3f159bb88d72b178dd2c1f9bd22fc0b291e2
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-08-09 19:24:41 -0300

    debug main menu

--- system/debug.gd
+++ system/debug.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://system/debu_2d.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")

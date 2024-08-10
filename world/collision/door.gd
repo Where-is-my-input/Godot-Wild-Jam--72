363c04a9ca2aaed60ddeac3b38aa24d67db4aac8
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-08-10 18:58:01 -0300

    trigger value

--- world/collision/door.gd
+++ world/collision/door.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 @export var world:Global.WORLD
+@export var triggerValue = 1
 
 func _ready():
 	Global.connect("platformTrigger", trigger)
 	Global.connect("topDownTrigger", trigger)
 
 func trigger(value):
-	queue_free()
+	if triggerValue == value:
+		queue_free()
--- world/interactables/button.gd
+++ world/interactables/button.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var world:Global.WORLD
+@export var triggerValue = 1
 
 func _on_area_2d_body_entered(body):
 	match world:
 		Global.WORLD.PLATFORMER:
-			Global.platformTrigger.emit(1)
+			Global.platformTrigger.emit(triggerValue)
 		Global.WORLD.TOPDOWN:
-			Global.topDownTrigger.emit(1)
+			Global.topDownTrigger.emit(triggerValue)

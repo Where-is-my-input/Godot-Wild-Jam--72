9693886941408c0875fd1f133850e66680195e50
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-08-10 17:01:15 -0300

    inputs

--- characters/player/CharacterBody2D.gd
+++ characters/player/CharacterBody2D.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+@onready var tmr_movement_cooldown = $tmrMovementCooldown
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,14 +15,17 @@ func _physics_process(delta):
 		#velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
-	if direction:
-		velocity = direction * SPEED
+	var direction = Vector2(Input.get_axis("topdown_left", "topdown_right"), Input.get_axis("topdown_up", "topdown_down"))
+	if direction && tmr_movement_cooldown.is_stopped():
+		#velocity = direction * SPEED
+		position.x += SPEED * direction.x
+		position.y += SPEED * direction.y
+		tmr_movement_cooldown.start(0.3)
 	else:
 		velocity = Vector2(move_toward(velocity.x, 0, SPEED), move_toward(velocity.y, 0, SPEED))
 
